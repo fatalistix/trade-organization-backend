a3c70ac3913ebb801bb2c09c2d45a303ef94bfea
fatalistix <slavabal11@outlook.com>
2024-04-18 20:10:05 +0700

    completed query

--- db/queries/000013_customer_by_product_and_period_and_trading_point.sql
+++ db/queries/000013_customer_by_product_and_period_and_trading_point.sql
@@ -1,1 +1,15 @@
-SELECT
+WITH r AS (
+    SELECT id, type
+    FROM receipt
+    WHERE created_at BETWEEN ? AND ?
+), t as (
+    SELECT receipt_id, receipt_type
+    FROM product_receipt
+    WHERE receipt_type = 'receipt_with_accounting'
+      AND product_id = ?
+      AND (receipt_id, receipt_type) IN (r)
+)
+SELECT customer_id
+FROM receipt_with_accounting
+WHERE (id, type) IN (t)
+GROUP BY customer_id;
