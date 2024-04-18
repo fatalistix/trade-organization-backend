53b64610a8f22870a8996eaf70f0883164e87d50
fatalistix <slavabal11@outlook.com>
2024-04-18 19:39:24 +0700

    added timestamp

--- db/migrations/000013_create_receipt_table.up.sql
+++ db/migrations/000013_create_receipt_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE receipt (
     id SERIAL UNIQUE NOT NULL,
     type TEXT NOT NULL CHECK ( type IN ('receipt_with_accounting', 'receipt_without_accounting') ),
+    created_at TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (id, type)
 );
