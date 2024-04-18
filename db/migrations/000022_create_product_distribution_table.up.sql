aa64faa1a2703e6aaa1655bc47449f546727d4c9
fatalistix <slavabal11@outlook.com>
2024-04-18 19:39:49 +0700

    added constraints for numeric type

--- db/migrations/000022_create_product_distribution_table.up.sql
+++ db/migrations/000022_create_product_distribution_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE product_distribution (
     distribution_id INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
     quantity INTEGER NOT NULL CHECK ( quantity >= 0 ),
-    price NUMERIC NOT NULL CHECK ( price >= 0 ),
+    price NUMERIC(18, 2) NOT NULL CHECK ( price >= 0 ),
     PRIMARY KEY (id),
     FOREIGN KEY (distribution_id) REFERENCES distribution(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
