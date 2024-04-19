a37ea1eb1da01b3d0fcf90b6bef9a354a26b2edc
fatalistix <slavabal11@outlook.com>
2024-04-19 17:23:07 +0700

    added created at timestamp

--- db/migrations/000025_create_order_table.up.sql
+++ db/migrations/000025_create_order_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "order" (
     id SERIAL UNIQUE NOT NULL,
     supplier_id INTEGER NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
     FOREIGN KEY (supplier_id) REFERENCES supplier(id)
 );
