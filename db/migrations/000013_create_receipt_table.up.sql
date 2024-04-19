519246bc086cd3844db53c6549aebe9f5daede72
fatalistix <slavabal11@outlook.com>
2024-04-19 17:07:04 +0700

    added not null constraint for timestamp

--- db/migrations/000013_create_receipt_table.up.sql
+++ db/migrations/000013_create_receipt_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE receipt (
     id SERIAL UNIQUE NOT NULL,
     type TEXT NOT NULL CHECK ( type IN ('receipt_with_accounting', 'receipt_without_accounting') ),
-    created_at TIMESTAMP DEFAULT NOW(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     seller_id INTEGER NOT NULL,
     PRIMARY KEY (id, type),
     FOREIGN KEY (seller_id) REFERENCES seller(id)
