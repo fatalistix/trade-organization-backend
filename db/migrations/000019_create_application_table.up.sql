d8e358efe6deac90f9a29190a06488c34fe828e8
fatalistix <slavabal11@outlook.com>
2024-04-19 17:07:20 +0700

    added created at timestamp

--- db/migrations/000019_create_application_table.up.sql
+++ db/migrations/000019_create_application_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE application (
     id SERIAL UNIQUE NOT NULL,
     trading_point_id INTEGER NOT NULL,
     trading_point_type TEXT NOT NULL CHECK ( trading_point_type IN ('department_store', 'store', 'kiosk', 'tray') ),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id),
     FOREIGN KEY (trading_point_id, trading_point_type) REFERENCES trading_point(id, type)
 );
