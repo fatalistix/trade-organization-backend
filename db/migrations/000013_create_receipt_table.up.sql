424ffc8a4c37dc3436ed01374b0c16c637d4288a
fatalistix <slavabal11@outlook.com>
2024-04-19 16:51:33 +0700

    added foreign key to seller

--- db/migrations/000013_create_receipt_table.up.sql
+++ db/migrations/000013_create_receipt_table.up.sql
@@ -2,5 +2,7 @@ CREATE TABLE receipt (
     id SERIAL UNIQUE NOT NULL,
     type TEXT NOT NULL CHECK ( type IN ('receipt_with_accounting', 'receipt_without_accounting') ),
     created_at TIMESTAMP DEFAULT NOW(),
-    PRIMARY KEY (id, type)
+    seller_id INTEGER NOT NULL,
+    PRIMARY KEY (id, type),
+    FOREIGN KEY (seller_id) REFERENCES seller(id)
 );
