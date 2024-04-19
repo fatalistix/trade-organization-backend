e3a2909d3777bba226e9993c7adad1e99b381b8b
fatalistix <slavabal11@outlook.com>
2024-04-19 17:22:54 +0700

    added price column

--- db/migrations/000026_create_product_order_table.up.sql
+++ db/migrations/000026_create_product_order_table.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE product_order (
     order_id INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
     quantity INTEGER NOT NULL CHECK ( quantity > 0 ),
+    price NUMERIC(18, 2) NOT NULL CHECK ( price >= 0 ),
     PRIMARY KEY (id),
     FOREIGN KEY (order_id) REFERENCES "order"(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
