ddd3b5a6aab281a26f34f7a9170ec53974e8f53e
fatalistix <slavabal11@outlook.com>
2024-04-22 17:35:00 +0700

    added status and nullable timestamp fields

--- db/migrations/000025_create_order_table.up.sql
+++ db/migrations/000025_create_order_table.up.sql
@@ -1,7 +1,16 @@
+CREATE TYPE order_status AS ENUM(
+    'completed',
+    'canceled',
+    'in_progress'
+);
+
 CREATE TABLE "order" (
     id SERIAL UNIQUE NOT NULL,
     supplier_id INTEGER NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    status order_status NOT NULL DEFAULT 'in_progress',
+    completed_at TIMESTAMP DEFAULT NULL,
+    canceled_at TIMESTAMP DEFAULT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (supplier_id) REFERENCES supplier(id)
 );
