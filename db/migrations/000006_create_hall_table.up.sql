2dcfdef0bf2cbf244f7abc73f011ef41c51a3c34
fatalistix <slavabal11@outlook.com>
2024-04-26 01:21:23 +0700

    added foreign key to trading point

--- db/migrations/000006_create_hall_table.up.sql
+++ db/migrations/000006_create_hall_table.up.sql
@@ -3,6 +3,8 @@ CREATE TABLE hall (
     type TEXT NOT NULL CHECK ( type = 'hall' ),
     hall_container_id INTEGER NOT NULL,
     hall_container_type TEXT NOT NULL CHECK ( hall_container_type IN ('section', 'store') ),
+    trading_point_id INTEGER NOT NULL,
+    trading_point_type TEXT NOT NULL CHECK ( trading_point_type IN ('department_store', 'store') ),
     PRIMARY KEY (id, type),
     FOREIGN KEY (id, type) REFERENCES place_of_work(id, type),
     FOREIGN KEY (hall_container_id, hall_container_type) REFERENCES hall_container(id, type)
