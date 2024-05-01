99c5394e42b4f91763f37c909704f9c9a0df5b66
fatalistix <slavabal11@outlook.com>
2024-05-01 15:22:15 +0700

    moved database manipulation to app, added env, config loading and app starting

--- cmd/trade-organization-backend/main.go
+++ cmd/trade-organization-backend/main.go
@@ -1,44 +1,64 @@
 package main
 
 import (
-	pgconnect "github.com/fatalistix/trade-organization-backend/internal/database/connection/postgres"
-	pgmigrate "github.com/fatalistix/trade-organization-backend/internal/database/migration/postgres"
-	"log"
+	"github.com/fatalistix/trade-organization-backend/internal/app"
+	"github.com/fatalistix/trade-organization-backend/internal/config"
+	"github.com/fatalistix/trade-organization-backend/internal/env"
+	slogattr "github.com/fatalistix/trade-organization-backend/internal/lib/log/slog/attr"
+	"github.com/golang-cz/devslog"
+	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	// init config
+	log := setupLogger()
 
-	// setup logger
+	env.MustLoadEnv()
+	log.Info("environment variables loaded")
 
-	// setup database
+	configPath := os.Getenv("CONFIG_PATH")
+	cfg := config.MustLoadConfig(configPath)
+	log.Info("config loaded")
+	log.Info("server info:", slog.Int("port", int(cfg.GRPC.Port)))
+	log.Debug("config", slog.Any("config", cfg))
 
-	// setup router
-
-	// run app
-
-	migrationFiles := os.DirFS("db")
-
-	db, err := pgconnect.NewDB(
-		"localhost",
-		5434,
-		"trade-organization-owner",
-		"trade-organization-owner",
-		"trade-organization",
-		pgconnect.SSLDisable,
-	)
+	a, err := app.NewApp(log, cfg)
 	if err != nil {
-		log.Fatal(err)
+		log.Error("unable to create app", slogattr.Err(err))
+		os.Exit(1)
 	}
 
-	migrator, err := pgmigrate.NewMigrator(migrationFiles, "migrations")
-	if err != nil {
-		log.Fatal(err)
+	log.Info("starting application")
+
+	go func() {
+		if err := a.Run(); err != nil {
+			log.Error("unable to run app", slogattr.Err(err))
+		}
+	}()
+
+	log.Info("application started")
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	<-stop
+
+	a.Stop()
+	log.Info("application stopped")
+}
+
+func setupLogger() *slog.Logger {
+	slogOpts := &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}
+	opts := &devslog.Options{
+		HandlerOptions:  slogOpts,
+		SortKeys:        true,
+		NewLineAfterLog: true,
 	}
 
-	err = migrator.ApplyMigrations(db, "trade-organization")
-	if err != nil {
-		log.Fatal(err)
-	}
+	return slog.New(devslog.NewHandler(os.Stdout, opts))
 }
