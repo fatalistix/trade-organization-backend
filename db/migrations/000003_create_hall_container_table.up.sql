69072083d37b3163aa6699c2b22f9b582aa33ea9
fatalistix <slavabal11@outlook.com>
2024-05-05 12:06:26 +0700

    fixed constraint: renamed: shop -> store

--- db/migrations/000003_create_hall_container_table.up.sql
+++ db/migrations/000003_create_hall_container_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE hall_container (
     id SERIAL UNIQUE NOT NULL,
-    type TEXT NOT NULL CHECK ( type in ('section', 'shop') ),
+    type TEXT NOT NULL CHECK ( type in ('section', 'store') ),
     PRIMARY KEY (id, type)
 );
