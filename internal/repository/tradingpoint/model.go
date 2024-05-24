456c9b0cae87674fa2d20670383551fc473560c0
fatalistix <slavabal11@outlook.com>
2024-05-24 13:12:15 +0700

    added more related db models

--- internal/repository/tradingpoint/model.go
+++ internal/repository/tradingpoint/model.go
@@ -2,8 +2,12 @@ package tradingpoint
 
 import (
 	"fmt"
-	modelcore "github.com/fatalistix/trade-organization-backend/internal/model/core"
-	model "github.com/fatalistix/trade-organization-backend/internal/model/tradingpoint"
+	modelcore "github.com/fatalistix/trade-organization-backend/internal/domain/model/core"
+	modelhall "github.com/fatalistix/trade-organization-backend/internal/domain/model/hall"
+	"github.com/fatalistix/trade-organization-backend/internal/domain/model/hall_container"
+	"github.com/fatalistix/trade-organization-backend/internal/domain/model/receipting_point_with_accounting"
+	modelsection "github.com/fatalistix/trade-organization-backend/internal/domain/model/section"
+	"github.com/fatalistix/trade-organization-backend/internal/domain/model/trading_point"
 )
 
 type tradingPoint struct {
@@ -15,18 +19,18 @@ type tradingPoint struct {
 	Address      string
 }
 
-func (t *tradingPoint) ToModel() (*model.TradingPoint, error) {
+func (t tradingPoint) ToModel() (trading_point.TradingPoint, error) {
 	const op = "repository.tradingpoint.ToModel"
 
-	mt, err := model.TypeFromString(t.Type)
+	mt, err := trading_point.TypeFromString(t.Type)
 	if err != nil {
-		return nil, fmt.Errorf("%s: unable to convert string to model type: %w", op, err)
+		return trading_point.TradingPoint{}, fmt.Errorf("%s: unable to convert string to model type: %w", op, err)
 	}
 	mm, err := modelcore.MoneyFromString(t.RentalCharge)
 	if err != nil {
-		return nil, fmt.Errorf("%s: unable to convert string to model money: %w", op, err)
+		return trading_point.TradingPoint{}, fmt.Errorf("%s: unable to convert string to model money: %w", op, err)
 	}
-	return &model.TradingPoint{
+	return trading_point.TradingPoint{
 		ID:           t.ID,
 		Type:         mt,
 		AreaPlot:     t.AreaPlot,
@@ -34,4 +38,67 @@ func (t *tradingPoint) ToModel() (*model.TradingPoint, error) {
 		CounterCount: t.CounterCount,
 		Address:      t.Address,
 	}, nil
+}
+
+type receiptingPointWithAccounting struct {
+	ID   int32
+	Type string
+}
+
+func (r receiptingPointWithAccounting) ToModel() (receipting_point_with_accounting.ReceiptingPointWithAccounting, error) {
+	const op = "repository.tradingpoint.ToModel"
+
+	mt, err := receipting_point_with_accounting.TypeFromString(r.Type)
+	if err != nil {
+		return receipting_point_with_accounting.ReceiptingPointWithAccounting{}, fmt.Errorf("%s: unable to convert string to model type: %w", op, err)
+	}
+
+	return receipting_point_with_accounting.ReceiptingPointWithAccounting{
+		ID:   r.ID,
+		Type: mt,
+	}, nil
+}
+
+type hall struct {
+	ID                int32
+	Type              string
+	HallContainerID   int32
+	HallContainerType string
+	TradingPointID    int32
+	TradingPointType  string
+}
+
+func (h hall) ToModel() (modelhall.Hall, error) {
+	const op = "repository.tradingpoint.ToModel"
+
+	hct, err := hall_container.TypeFromString(h.HallContainerType)
+	if err != nil {
+		return modelhall.Hall{}, fmt.Errorf("%s: unable to convert string to model type: %w", op, err)
+	}
+
+	tpt, err := trading_point.TypeFromString(h.TradingPointType)
+	if err != nil {
+		return modelhall.Hall{}, fmt.Errorf("%s: unable to convert string to model type: %w", op, err)
+	}
+
+	return modelhall.Hall{
+		ID:                h.ID,
+		HallContainerID:   h.HallContainerID,
+		HallContainerType: hct,
+		TradingPointID:    h.TradingPointID,
+		TradingPointType:  tpt,
+	}, nil
+}
+
+type section struct {
+	ID                int32
+	Type              string
+	DepartmentStoreID int32
+}
+
+func (s section) ToModel() modelsection.Section {
+	return modelsection.Section{
+		ID:                s.ID,
+		DepartmentStoreID: s.DepartmentStoreID,
+	}
 }
