356b62fe64f081110957abeac9c364f82dad1c03
fatalistix <slavabal11@outlook.com>
2024-05-24 13:06:38 +0700

    date representation

---
+++ internal/domain/model/core/date.go
@@ -0,0 +1,26 @@
+package core
+
+import "fmt"
+
+type Date struct {
+	Year  int32
+	Month int32
+	Day   int32
+}
+
+func (d Date) String() string {
+	return fmt.Sprintf("%04d-%02d-%02d", d.Year, d.Month, d.Day)
+}
+
+func DateFromString(date string) (Date, error) {
+	const op = "domain.model.core.DateFromString"
+
+	d := Date{}
+
+	_, err := fmt.Sscanf(date, "%04d-%02d-%02d", &d.Year, &d.Month, &d.Day)
+	if err != nil {
+		return Date{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return d, nil
+}
