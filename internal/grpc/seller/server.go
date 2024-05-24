0d249090cf14f443eafbce8d805b05e93a93cd7f
fatalistix <slavabal11@outlook.com>
2024-05-24 13:11:06 +0700

    added handle function

--- internal/grpc/seller/server.go
+++ internal/grpc/seller/server.go
@@ -2,45 +2,42 @@ package seller
 
 import (
 	"context"
+	"github.com/fatalistix/trade-organization-backend/internal/grpc/seller/handler/list"
+	"github.com/fatalistix/trade-organization-backend/internal/grpc/seller/handler/register"
+	"github.com/fatalistix/trade-organization-backend/internal/grpc/seller/handler/update"
+	repository "github.com/fatalistix/trade-organization-backend/internal/repository/seller"
 	proto "github.com/fatalistix/trade-organization-proto/gen/go/seller"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"log/slog"
 )
 
 type ServerAPI struct {
 	proto.UnimplementedSellerServiceServer
-	log *slog.Logger
+	registerHandlerFunc register.HandlerFunc
+	listHandlerFunc     list.HandlerFunc
+	updateHandlerFunc   update.HandlerFunc
 }
 
-func RegisterServer(gRPC *grpc.Server, log *slog.Logger) {
+func RegisterServer(gRPC *grpc.Server, log *slog.Logger, repository *repository.Repository) {
 	proto.RegisterSellerServiceServer(
 		gRPC,
 		&ServerAPI{
-			log: log,
+			registerHandlerFunc: register.MakeRegisterHandlerFunc(log, repository),
+			listHandlerFunc:     list.MakeListHandlerFunc(log, repository),
+			updateHandlerFunc:   update.MakeUpdateHandlerFunc(log, repository),
 		},
 	)
 }
 
 func (s *ServerAPI) Register(ctx context.Context, req *proto.RegisterRequest) (*proto.RegisterResponse, error) {
-	const op = "grpc.seller.Register"
-
-	s.log.With(
-		slog.String("op", op),
-	)
-
-	s.log.Info("request encoded", slog.Any("request", req.BirthDate))
-
-	return &proto.RegisterResponse{}, nil
+	return s.registerHandlerFunc(ctx, req)
 }
 
-func (s *ServerAPI) ListByTradingPoint(ctx context.Context, req *proto.ListByTradingPointRequest) (*proto.ListByTradingPointResponse, error) {
-	const op = "grpc.seller.ListByTradingPoint"
+func (s *ServerAPI) List(ctx context.Context, req *proto.ListRequest) (*proto.ListResponse, error) {
+	return s.listHandlerFunc(ctx, req)
+}
 
-	s.log.With(
-		slog.String("op", op),
-	)
-
-	s.log.Info("request encoded", slog.Any("request", req))
-
-	return &proto.ListByTradingPointResponse{}, nil
+func (s *ServerAPI) Update(ctx context.Context, req *proto.UpdateRequest) (*emptypb.Empty, error) {
+	return s.updateHandlerFunc(ctx, req)
 }
