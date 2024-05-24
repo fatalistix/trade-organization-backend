f09598e2e0c86f6693e17a6f029138a71f10fe7a
fatalistix <slavabal11@outlook.com>
2024-05-24 13:05:24 +0700

    added check and default values

--- db/migrations/000012_create_seller_table.up.sql
+++ db/migrations/000012_create_seller_table.up.sql
@@ -5,17 +5,20 @@ CREATE TYPE seller_status AS ENUM(
 
 CREATE TABLE seller (
     id SERIAL UNIQUE NOT NULL,
-    status seller_status NOT NULL,
+    status seller_status NOT NULL DEFAULT 'working',
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     middle_name TEXT NOT NULL,
     birth_date DATE NOT NULL,
     salary NUMERIC(18, 2) NOT NULL CHECK ( salary >= 0 ),
     phone_number TEXT NOT NULL,
-    place_of_work_id INTEGER,
-    place_of_work_type TEXT CHECK ( place_of_work_type IN ('hall', 'tray', 'kiosk', NULL) ),
-    trading_point_id INTEGER,
-    trading_point_type TEXT CHECK ( trading_point_type IN ('department_store', 'store', 'tray', 'kiosk', NULL) ),
+    place_of_work_id INTEGER DEFAULT NULL,
+    place_of_work_type TEXT CHECK ( place_of_work_type IN ('hall', 'tray', 'kiosk', NULL) ) DEFAULT NULL,
+    trading_point_id INTEGER DEFAULT NULL,
+    trading_point_type TEXT CHECK ( trading_point_type IN ('department_store', 'store', 'tray', 'kiosk', NULL) ) DEFAULT NULL,
+    CHECK (
+        (place_of_work_id IS NULL AND place_of_work_type IS NULL AND trading_point_id IS NULL AND trading_point_type IS NULL) OR
+        (place_of_work_id IS NOT NULL AND place_of_work_type IS NOT NULL AND trading_point_id IS NOT NULL AND trading_point_type IS NOT NULL)),
     PRIMARY KEY (id),
     UNIQUE (phone_number),
     FOREIGN KEY (place_of_work_id, place_of_work_type) REFERENCES place_of_work(id, type),
